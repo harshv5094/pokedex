c26689a9ca9dedd57677218c0a5343546e4fa318
harshv5094 (Local User) <harshv5094@gmail.com>
2024-02-15 10:28:49 +0530

    feat: Fetching Pokemon Details from `PokeAPI`

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,50 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Button, Grid, GridItem, Heading, Link } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Home() {
+  const quantity = 20
+  const [pokeArray, setPokeArray] = useState([])
+
+  const getAllPokemon = async quantity => {
+    let pokeArray = []
+    for (let i = 1; i <= quantity; i++) {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${i}`
+        )
+        pokeArray.push(response.data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    setPokeArray(pokeArray)
+  }
+
+  useEffect(() => {
+    getAllPokemon(quantity)
+  }, [quantity])
+
   return (
     <Box>
-      <Heading>Home Page</Heading>
+      <Heading m={'10px'} textAlign={'center'}>
+        Home Page
+      </Heading>
+
+      <Grid templateColumns={'repeat(3, 2fr)'} gap={5}>
+        {pokeArray.map((pokemon, index) => {
+          return (
+            <GridItem key={index}>
+              <Heading>{pokemon.name}</Heading>
+              <Link href={`/pokemon/${pokemon.id}`}>
+                <Button colorScheme='green' size={`md`}>
+                  View Details
+                </Button>
+              </Link>
+            </GridItem>
+          )
+        })}
+      </Grid>
     </Box>
   )
 }
--- src/pages/PokemonInfo.jsx
+++ src/pages/PokemonInfo.jsx
@@ -1,9 +1,13 @@
 import { Box, Heading } from '@chakra-ui/react'
+import { useParams } from 'react-router-dom'
 
 function PokemonInfo() {
+  let { pokemonID } = useParams()
+
   return (
     <Box>
       <Heading>Pokemon Info Page</Heading>
+      {pokemonID}
     </Box>
   )
 }
